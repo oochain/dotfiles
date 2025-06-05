b2b5a760a5347e9e5d40f6a5fc2ad71ab3fdbf29
oochain <chenwu.it@gmail.com>
2025-06-05 10:56:20 +0000

    nvim: change tab width to 4 for json/js/ts files

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,11 +2,19 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters = {
+        prettier = {
+          prepend_args = { "--tab-width", "4" },
+        },
+      },
       formatters_by_ft = {
         python = { "ruff" },
         terraform = { "terraform_fmt" },
         tf = { "terraform_fmt" },
         ["terraform-vars"] = { "terraform_fmt" },
+        json = { "prettier" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
       },
     },
   },
