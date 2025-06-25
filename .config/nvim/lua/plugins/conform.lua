e43f859fc6d6ede3175c5be45aace829aa729c2a
oochain <chenwu.it@gmail.com>
2025-06-25 02:20:08 +0000

    Add html/js/json support with 2 tab width

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,8 +3,10 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters = {
-        prettier = {
-          prepend_args = { "--tab-width", "4" },
+        prettier_2_spaces = {
+          command = "prettier",
+          args = { "--tab-width", "2", "--stdin-filepath", "$FILENAME" },
+          stdin = true,
         },
       },
       formatters_by_ft = {
@@ -12,9 +14,12 @@ return {
         terraform = { "terraform_fmt" },
         tf = { "terraform_fmt" },
         ["terraform-vars"] = { "terraform_fmt" },
-        json = { "prettier" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
+        json = { "prettier_2_spaces" },
+        javascript = { "prettier_2_spaces" },
+        typescript = { "prettier_2_spaces" },
+        html = { "prettier_2_spaces" },
+        css = { "prettier_2_spaces" },
+        scss = { "prettier_2_spaces" },
       },
     },
   },
