01f492f068b07a2fcb36c6f72d3b1e6d53105b2c
oochain <chenwu.it@gmail.com>
2025-07-03 00:00:15 +0000

    Disable the length 80 check for markdown lint

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,3 +1,4 @@
+local HOME = os.getenv("HOME")
 return {
   {
     "stevearc/conform.nvim",
@@ -8,6 +9,20 @@ return {
       formatters = {
         markdownlint = {
           prepend_args = { "--indent", "4" },
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = { "--config", HOME .. "/.markdownlint-cli2.yaml", "--" },
+          -- to disable lingth check, add the following in ~/prod-main-dashboard.json
+          -- config:
+          --  MD013: false
         },
       },
     },
