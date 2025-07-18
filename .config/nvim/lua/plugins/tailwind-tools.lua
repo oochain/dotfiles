2358aae911cd06ededc802a5ef44518a836b94c2
oochain <chenwu.it@gmail.com>
2025-07-18 02:40:33 +0000

    nvim: enable tailwind v4 language server

---
+++ .config/nvim/lua/plugins/tailwind-tools.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "tailwindlabs/tailwindcss-language-server",
+    enabled = false,
+  },
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {},
+  },
+}
