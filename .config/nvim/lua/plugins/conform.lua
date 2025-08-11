8037f2f1948f5193f29d7396d4fcd9fae0334fae
oochain <chenwu.it@gmail.com>
2025-08-11 03:16:16 +0000

    nvim: cleanup .prettier config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,24 +2,17 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      formatters = {
-        prettier_2_spaces = {
-          command = "prettier",
-          args = { "--tab-width", "2", "--stdin-filepath", "$FILENAME" },
-          stdin = true,
-        },
-      },
       formatters_by_ft = {
         python = { "ruff" },
         terraform = { "terraform_fmt" },
         tf = { "terraform_fmt" },
         ["terraform-vars"] = { "terraform_fmt" },
-        json = { "prettier_2_spaces" },
-        javascript = { "prettier_2_spaces" },
-        typescript = { "prettier_2_spaces" },
-        html = { "prettier_2_spaces" },
-        css = { "prettier_2_spaces" },
-        scss = { "prettier_2_spaces" },
+        json = { "prettier" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
       },
     },
   },
