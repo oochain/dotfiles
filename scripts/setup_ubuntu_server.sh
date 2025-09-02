4ec655c66430452f50bd9d5ee35daa5f0796b228
oochain <chenwu.it@gmail.com>
2025-09-02 23:10:17 +0000

    add(scripts): need nethogs to monitor network traffic

--- scripts/setup_ubuntu_server.sh
+++ scripts/setup_ubuntu_server.sh
@@ -39,7 +39,7 @@ CORE_PACKAGES="build-essential libreadline-dev libssl-dev"
 UTIL_PACKAGES="curl wget unzip unrar jq tree \
     fail2ban network-manager \
     fonts-noto-cjk language-pack-zh-hans xfonts-wqy \
-    moreutils pwgen sqlite3 xclip"
+    moreutils pwgen sqlite3 xclip nethogs"
 PYTHON_PACKAGES="clang python3-pip python3-venv pipx"
 NVIM_PACKAGES="fd-find lua5.1 luarocks fish fzf"
 
