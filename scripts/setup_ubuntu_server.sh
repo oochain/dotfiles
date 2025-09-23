47bc722964ed1039bd679de4bef5d76dbee4299e
oochain <chenwu.it@gmail.com>
2025-09-23 23:53:22 +0000

    update(scripts): Lazyvim now needs neovim 0.11+. Auto reinstall new version

--- scripts/setup_ubuntu_server.sh
+++ scripts/setup_ubuntu_server.sh
@@ -83,15 +83,13 @@ fi
 # Install ruff
 curl -LsSf https://astral.sh/ruff/install.sh | sh
 
-# Install Neovim
-if ! command_exists nvim; then
-	echo "Installing Neovim..."
-	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-	sudo rm -rf /opt/nvim
-	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-else
-	echo "Neovim already installed"
-fi
+# Install/Update Neovim to v0.11.4
+echo "Installing/Updating Neovim to v0.11.4..."
+sudo rm -rf /opt/nvim /opt/nvim-linux-x86_64 # Remove old versions
+curl -LO https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
+echo "Neovim v0.11.4 installed."
 
 # Install Terraform
 if ! command_exists terraform; then
