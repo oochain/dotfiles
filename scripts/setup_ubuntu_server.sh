4f2e00ae5eee9443b8fcbf73b007b766110f5c51
oochain <chenwu.it@gmail.com>
2026-09-07 23:08:41 +0000

    script: add missing pkg for ssh over ssm
    Missing unbuffer and on Ubuntu / Debian Linux, unbuffer is part of
    the expect package.

--- scripts/setup_ubuntu_server.sh
+++ scripts/setup_ubuntu_server.sh
@@ -36,7 +36,7 @@ fi
 
 # Package groups
 CORE_PACKAGES="build-essential libreadline-dev libssl-dev"
-UTIL_PACKAGES="curl wget unzip unrar jq tree \
+UTIL_PACKAGES="curl wget unzip unrar jq tree expect \
     fail2ban fonts-noto-cjk language-pack-zh-hans xfonts-wqy \
     moreutils pwgen sqlite3 xclip nethogs"
 NVIM_PACKAGES="fd-find"
